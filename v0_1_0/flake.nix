bb385c28e1d4d7343d522e65798a0c6191372ed4
hugosenari <hugosenari@gmail.com>
2022-03-17 18:25:45 -0300

    chore: re-index ../nimpkgs/h/haraka/v0_1_0

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -11,7 +11,7 @@
   inputs.src-haraka-v0_1_0.owner = "~ehmry";
   inputs.src-haraka-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-haraka-v0_1_0.repo  = "nim_haraka";
-  inputs.src-haraka-v0_1_0.type  = "other";
+  inputs.src-haraka-v0_1_0.type  = "sourcehut";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
